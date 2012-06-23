ac43f76016bc35c754c11facc7a91662361b0367
Daniel Chiu <dwchiu@stanford.edu>
2012-06-23 16:56:18 -0700

    Added validation to trips model.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
   attr_accessible :date, :price, :start, :stop, :user_id
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :start, :stop, :date, :price, presence: true
 end
