16c22820d3b58e7d88a73efb9777626bf307a674
Edward Lando <edwardlando8@gmail.com>
2012-06-23 14:18:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carquester::Application.config.secret_token = '43b802431c620bf30fe7d52f83d10d7383c0b328ba45ae92bc9d692bdca1ef94073eb7aa466382aced72d66d8a057433e5ed2228d0ca653b46eb484d870cb5bb'
