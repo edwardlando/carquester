020badfb1c20273f1dbdc3c53260260bd43270bd
Edward Lando <edwardlando8@gmail.com>
2012-06-23 20:02:25 -0700

    photo for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120624020259) do
+ActiveRecord::Schema.define(:version => 20120624023443) do
 
   create_table "reviews", :force => true do |t|
     t.string   "content"
@@ -48,6 +48,10 @@ ActiveRecord::Schema.define(:version => 20120624020259) do
     t.decimal  "points",                 :default => 0.0
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
