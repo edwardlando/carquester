6ef80d1141abb5b75d74818eeb5ea2b65712526e
Edward Lando <edwardlando8@gmail.com>
2012-06-23 17:40:54 -0700

    updated users table

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,9 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
+  
+  has_many :users, :foreign_key => :passengers
+  
+  
   attr_accessible :date, :price, :start, :stop, :user_id
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :start, :stop, :date, :price, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120624001526) do
+ActiveRecord::Schema.define(:version => 20120624003541) do
 
   create_table "reviews", :force => true do |t|
     t.string   "content"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(:version => 20120624001526) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.decimal  "points"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
